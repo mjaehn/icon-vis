677f579ccf7f24b734bbe8e188c7702ce4195dd0
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-11 15:34:32 +0200

    test eccodes daint env

--- env/create_environment.sh
+++ env/create_environment.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# load python3
-if [[ $slave == 'daint' ]]; then 
-	module load daint-gpu cray-python
-elif [[ $slave == 'tsa' ]]; then 
-	module load python/3.7.4
-fi
 
 PROJECT_FOLDER=/project/g110/pyvis
 rm -rf ${PROJECT_FOLDER}
@@ -15,15 +9,16 @@ if [[ $slave == 'daint' ]]; then
 	module load EasyBuild-custom
 	eb GEOS-3.10.2-CrayGNU-21.09-python3.eb -r
 	eb Eigen-3.4.0-CrayGNU-21.09.eb -r
-   	module load GEOS Eigen PROJ Boost GSL
+	module load daint-gpu cray-python
+   	module load GEOS Eigen PROJ Boost GSL ecCodes/2.23.0-CrayGNU-21.09
 elif [[ $slave == 'tsa' ]]; then 
 	module use /apps/common/UES/sandbox/kraushm/tsa-PROJ/modules/all
-	module load PrgEnv-gnu
-	module load proj/8.0.0-fosscuda-2019b
-	module load geos
+	module load python/3.7.4
+	module load PrgEnv-gnu 
+	module load proj/8.0.0-fosscuda-2019b geos
 fi
 
-VENV_PATH=${PROJECT_FOLDER}/venv_$slave
+VENV_PATH=${PROJECT_FOLDER}/venv_${slave}_test
 
 rm -rf $VENV_PATH
 mkdir -p ${VENV_PATH}
--- load_env.sh
+++ load_env.sh
@@ -22,20 +22,6 @@ if [[ $HOST == *'tsa'* ]]; then
     eccodes=`spack location -i eccodes@2.19.0%gcc@8.3.0`
     export ECCODES_DIR=${eccodes}
 
-elif [[ $HOST == *'daint'* ]]; then
-    echo 'Setting GRIB_DEFINITION_PATH for cf-grib engine'
-
-    module load cray-python
-    source /project/g110/spack/user/daint/spack/share/spack/setup-env.sh
-
-    cosmo_eccodes=`spack location -i cosmo-eccodes-definitions@2.19.0.7%gcc@8.3.0`
-    eccodes=`spack location -i eccodes@2.19.0%gcc@8.3.0+build_shared_libs`
-
-    export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
-    export OMPI_MCA_pml="ucx" 
-    export OMPI_MCA_osc="ucx"
-    export ECCODES_DIR=${eccodes}
-
 fi
 
 if [[ $HOST == *'tsa'* ]]; then
@@ -54,7 +40,7 @@ if [[ $HOST == *'tsa'* ]]; then
     source /project/g110/pyvis/venv_tsa/bin/activate
 elif [[ $HOST == *'daint'* ]]; then
     export EASYBUILD_PREFIX=/project/g110/pyvis
-    module load daint-gpu EasyBuild-custom PROJ GEOS cray-python
+    module load daint-gpu EasyBuild-custom PROJ GEOS cray-python ecCodes/2.23.0-CrayGNU-21.09
     source /project/g110/pyvis/venv_daint/bin/activate
 fi
 
