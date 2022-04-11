1d2d886a01a06c447fdbc64245073dbbbf42856b
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-11 15:56:56 +0200

    move up module load python

--- env/create_environment.sh
+++ env/create_environment.sh
@@ -5,15 +5,15 @@ rm -rf ${PROJECT_FOLDER}
 mkdir -p ${PROJECT_FOLDER}
 
 if [[ $slave == 'daint' ]]; then 
+	module load daint-gpu cray-python
 	export EASYBUILD_PREFIX=${PROJECT_FOLDER}
 	module load EasyBuild-custom
 	eb GEOS-3.10.2-CrayGNU-21.09-python3.eb -r
 	eb Eigen-3.4.0-CrayGNU-21.09.eb -r
-	module load daint-gpu cray-python
    	module load GEOS Eigen PROJ Boost GSL ecCodes/2.23.0-CrayGNU-21.09
 elif [[ $slave == 'tsa' ]]; then 
-	module use /apps/common/UES/sandbox/kraushm/tsa-PROJ/modules/all
 	module load python/3.7.4
+	module use /apps/common/UES/sandbox/kraushm/tsa-PROJ/modules/all
 	module load PrgEnv-gnu 
 	module load proj/8.0.0-fosscuda-2019b geos
 fi
