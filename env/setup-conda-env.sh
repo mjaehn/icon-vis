34dd839ad1e8c05cde4047f3d164ac1d60d51960
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2022-04-12 11:31:45 +0200

    eccodes spack test tsa

--- env/setup-conda-env.sh
+++ env/setup-conda-env.sh
@@ -4,16 +4,25 @@
 
 if [[ $HOST == *'tsa'* ]]; then
     echo 'Loading modules for cf-grib engine'
-    source ~osm/.opr_setup_dir
-    export PATH=$OPR_SETUP_DIR/bin:$PATH
-    export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
+    # source ~osm/.opr_setup_dir
+    # export PATH=$OPR_SETUP_DIR/bin:$PATH
+    # export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
     
-    module load PrgEnv-gnu/19.2
-    module load eccodes/2.19.0-gnu-8.3.0-nocuda-noomp
-    module load eccodes_cosmo_resources/2.19.0.5
+    # module load PrgEnv-gnu/19.2
+    # module load eccodes/2.19.0-gnu-8.3.0-nocuda-noomp
+    # module load eccodes_cosmo_resources/2.19.0.5
+    module load python
+    source /project/g110/spack/user/tsa/spack/share/spack/setup-env.sh
 
+    cosmo_eccodes=`spack location -i cosmo-eccodes-definitions@2.19.0.7%gcc@8.3.0`
+    eccodes=`spack location -i eccodes@2.19.0%gcc@8.3.0`
+
+    export GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
+    export OMPI_MCA_pml="ucx" 
+    export OMPI_MCA_osc="ucx"
     echo ${GRIB_DEFINITION_PATH}
     conda env config vars set GRIB_DEFINITION_PATH=${GRIB_DEFINITION_PATH}
+
 elif [[ $HOST == *'daint'* ]]; then
     echo 'Setting GRIB_DEFINITION_PATH for cf-grib engine'
 
@@ -28,6 +37,7 @@ elif [[ $HOST == *'daint'* ]]; then
     export OMPI_MCA_osc="ucx"
     echo ${GRIB_DEFINITION_PATH}
     conda env config vars set GRIB_DEFINITION_PATH=${cosmo_eccodes}/cosmoDefinitions/definitions/:${eccodes}/share/eccodes/definitions/
+    
 fi
 
 # ---- required for fieldextra ------
