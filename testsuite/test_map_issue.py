2c03058877d1b1f787070169c7aa8b4719c542db
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2022-04-21 13:55:56 +0200

    Use pathlib as in real example

--- testsuite/test_map_issue.py
+++ testsuite/test_map_issue.py
@@ -1,18 +1,11 @@
 import psyplot.project as psy
 import os
+from pathlib import Path
 
 
 def test_map_issue():
-    cmd = 'python data/get_data.py'
-    os.popen(cmd)
-    ds = psy.open_dataset(
-        'data/example_data/nc/my_exp1_atm_3d_ml_20180921T000000Z.nc')
-    pp = psy.plot.mapplot(ds,
-                          name='temp',
-                          t=0,
-                          bounds={
-                              'method': 'minmax',
-                              'vmin': 260.0,
-                              'vmax': 300.0
-                          })
+    os.popen('python data/get_data.py')
+    input_file = Path('data/example_data/nc/my_exp1_atm_3d_ml_20180921T000000Z.nc')
+    ds = psy.open_dataset(input_file)
+    pp = psy.plot.mapplot(ds, name='temp', t=0, bounds={'method': 'minmax', 'vmin': 260.0, 'vmax': 300.0})
     pp.update(projection='robin')
