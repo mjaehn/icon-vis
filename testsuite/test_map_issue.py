ee8973a4ab92cbe4a11878a079097cd58157063f
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2022-04-21 16:44:28 +0200

    Change order

--- testsuite/test_map_issue.py
+++ testsuite/test_map_issue.py
@@ -8,12 +8,10 @@ def test_map_issue():
     input_file = Path(
         'data/example_data/nc/my_exp1_atm_3d_ml_20180921T000000Z.nc')
     ds = psy.open_dataset(input_file)
-    pp = psy.plot.mapplot(ds,
-                          name='temp',
-                          t=0,
+    pp = ds.psy.plot.mapplot(name='temp')
+    pp.update(t=0,
                           bounds={
                               'method': 'minmax',
                               'vmin': 260.0,
                               'vmax': 300.0
                           })
-    pp.update(projection='robin')
